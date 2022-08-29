8b524b2b8a55a481c2af9e20c00cd67710c6bee0
CarmenXP <xocuaposadas.carmen@gmail.com>
2022-08-29 17:54:54 -0500

    adding form

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -22,15 +22,18 @@ const Home = () => {
 
   return (
     <div className="home">
-      <div className="poke_img">
-          <img src="https://e.rpp-noticias.io/normal/2016/07/22/151715_200529.jpg"/>
-      </div>
-      <h2>Hola entrenador</h2>
-      <p>Para poder comenzar, dame tu nombre</p>
-      <form className="enter_name" onSubmit={hanledSubmit}>
-          <input type="text" placeholder='Enter name' id='name'/>
-          <button>Comenzar</button>
-      </form>
+        <div className="poke_img">
+          <img  src='https://i.pinimg.com/564x/79/d0/e8/79d0e8827e71864c6f78d4820936351f.jpg'/>
+        </div>
+        <div className="content_form">
+          <h1>Welcome to The Pokedex</h1>
+          <p>Please! Enter your name to start</p>
+          <form className="enter_name" onSubmit={hanledSubmit}>
+              <input type="text" placeholder='Enter your name' id='name'/>
+              <button><i className='bx bxs-right-arrow'></i></button>
+          </form>
+        </div>
+      
     </div>
   )
 }
