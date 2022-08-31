5735ed50042d5edbb78919a17b88adb8e643766d
CarmenXP <xocuaposadas.carmen@gmail.com>
2022-08-31 18:54:35 -0500

    detail page styles

--- src/components/PokedexDetail.jsx
+++ src/components/PokedexDetail.jsx
@@ -20,21 +20,21 @@ const PokedexDetail = () => {
         <div className='poke_detail'>
           
           <img src={pokeInfo?.sprites.other['official-artwork'].front_default} alt=""  />
-          <h1>{pokeInfo?.name.toUpperCase()}</h1>
-          <ul>
+          <h1 className={`text_${pokeInfo?.types[0].type.name}`}>{pokeInfo?.name.toUpperCase()}</h1>
+          <ul className='poke_list'>
             <li>
               <span>{pokeInfo?.weight}</span>
-              <h3>Weight</h3>
+              <h3 className={`text_${pokeInfo?.types[0].type.name}`}>Weight</h3>
             </li>
             <li>
               <span>{pokeInfo?.height}</span>
-              <h3>Height</h3>
+              <h3 className={`text_${pokeInfo?.types[0].type.name}`}>Height</h3>
             </li>
           </ul>
           
-          <ul>
+          <ul className='poke_list'>
             <li>
-              <h3>Type</h3>
+              <h3 className={`text_${pokeInfo?.types[0].type.name}`}>Type</h3>
               {
                 pokeInfo?.types.map(x =>(
                   <span key={x.type.name}>{x.type.name }</span>
@@ -42,9 +42,9 @@ const PokedexDetail = () => {
               }
             </li>
           </ul>
-          <ul>
+          <ul className='poke_list'>
             <li>
-              <h3>Abilities</h3>
+              <h3 className={`text_${pokeInfo?.types[0].type.name}`}>Abilities</h3>
               {
                 pokeInfo?.abilities.map(x =>(
                   <span key={x.ability.name}>{x.ability.name }</span>
@@ -52,6 +52,28 @@ const PokedexDetail = () => {
               }
             </li>
           </ul>
+        </div>
+        <div className="poke_stats">
+          <h2 className={`text_${pokeInfo?.types[0].type.name}`}>STATS</h2>
+          {
+            pokeInfo?.stats.map(x =>(
+              <div className='progress_bar'>
+                <label htmlFor={x.stat.name}><strong>{x.stat.name}</strong></label>
+                <progress className='progress' id={x.stat.name} max="150" value={x['base_stat']}>{x['base_stat']}</progress>
+              </div>
+         
+            ))
+          }
+        </div>
+        <h2 className={` move text_${pokeInfo?.types[0].type.name}`}>MOVES</h2>
+        <div className="poke_movements">
+          
+          <div className="moves"></div>
+          {
+            pokeInfo?.moves.map(x =>(
+              <span>{x.move.name}</span>
+            ))
+            } 
         </div>
     </div>
 
