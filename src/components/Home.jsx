72fb6118821ce4f5c07f6420c0ae95f376d7afce
CarmenXP <xocuaposadas.carmen@gmail.com>
2022-08-31 22:38:36 -0500

    css fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import {useState}from 'react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { setNameTrainer } from '../store/slices/nameTrainer.slice'
 
 const Home = () => {
+
+  const [isOpen, setIsOpen] = useState(false)
 
   const dispatch =useDispatch()
   const navigate = useNavigate()
@@ -17,7 +19,11 @@ const Home = () => {
       navigate('/pokedex')
     }
     e.target.name.value =''
+    handleCloseModal()
   }
+
+  const handleOpenModal = () => setIsOpen(true)
+  const handleCloseModal = () => setIsOpen(false)
 
 
   return (
@@ -25,15 +31,15 @@ const Home = () => {
         <div className="poke_img">
           <img  src='https://i.pinimg.com/564x/79/d0/e8/79d0e8827e71864c6f78d4820936351f.jpg'/>
         </div>
-        <div className="content_form">
+        <div className={isOpen ? "content_form" : "content_none" } >
           <h1>Welcome to The Pokedex</h1>
           <p>Please! Enter your name to start</p>
           <form className="enter_name" onSubmit={hanledSubmit}>
               <input type="text" placeholder='Enter your name' id='name'/>
-              <button><i className='bx bxs-right-arrow'></i></button>
+              <button className='btn_send'><i className='bx bxs-right-arrow'></i></button>
           </form>
         </div>
-      
+        <button className={isOpen? 'content_none':'btn_press'} onClick={handleOpenModal}>Press to enter</button>
     </div>
   )
 }
